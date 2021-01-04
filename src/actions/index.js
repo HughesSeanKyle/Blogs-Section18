47516f1702f622faf79ff217ba8adda4d1ce9eed
HughesSeanKyle <khughessean@yahoo.com>
2021-01-04 14:56:17 +0200

    V254 - Understanding Async Action Creators

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,7 +1,6 @@
 import jsonPlaceholder from '../apis/jsonPlaceholder'; 
 
-export const fetchPosts = async () => {
-    // Bad approach! Will get error // 1
+export const fetchPosts = async () => {// 1
     const response = await jsonPlaceholder.get('/posts');
 
     return {
@@ -17,5 +16,10 @@ First error
 
 Second error
 - Use custom middleware for async actions
+ 
+What is wrong with 'fetchPosts'?
+Action creators must return plain JS objects with a type property - we are not!
+
+By the time our action gets to a reducer, we won't have fetched our data. 
 */
 
