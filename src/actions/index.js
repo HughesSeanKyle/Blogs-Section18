7efda30cb40526b39a175819efa90d4042aa64af
HughesSeanKyle <khughessean@yahoo.com>
2021-01-07 12:59:11 +0200

    V274 - Fetching Singular Records

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,10 +1,16 @@
 import jsonPlaceholder from '../apis/jsonPlaceholder'; 
 
 export const fetchPosts = () => async dispatch => { // 1.1
-        const response = await jsonPlaceholder.get('/posts');
+    const response = await jsonPlaceholder.get('/posts');
 
-        dispatch({ type: 'FETCH_POSTS', payload: response.data } )
-    };
+    dispatch({ type: 'FETCH_POSTS', payload: response.data } )
+};
+
+export const fetchUser = () => async dispatch => {
+    const response = await jsonPlaceholder.get(`/users/${id}`);
+
+    dispatch({ type: 'FETCH_USER', payload: response.data })
+};
 
 /*
 // 1
