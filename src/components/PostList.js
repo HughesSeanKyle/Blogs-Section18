9d3eb2ad58f39b68e920394c0553c7ff9779f200
HughesSeanKyle <khughessean@yahoo.com>
2021-01-08 13:38:43 +0200

    V283 - Action Creators in Action Creators

--- src/components/PostList.js
+++ src/components/PostList.js
@@ -1,12 +1,12 @@
 import userEvent from '@testing-library/user-event';
 import React from 'react';
 import { connect } from 'react-redux';
-import { fetchPosts } from '../actions';
+import { fetchPostsAndUsers } from '../actions';
 import UserHeader from './UserHeader';
 
 class PostList extends React.Component {
     componentDidMount() {
-        this.props.fetchPosts();
+        this.props.fetchPostsAndUsers();
     }
 
     renderList() {
@@ -36,7 +36,7 @@ const mapStateToProps = (state) => {
     return { posts: state.posts };
 }
 
-export default connect(mapStateToProps, { fetchPosts })(PostList); // 1
+export default connect(mapStateToProps, { fetchPostsAndUsers })(PostList); // 1
 
 /*
 **General Data Loading with Redux
